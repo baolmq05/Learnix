784fcf0eb7a62171f16d9aaea969ff8e721bf21d
Huy Ben <benpham06@gmail.com>
2025-11-06 16:18:11 +0700

    [Huy-Ben] commit

--- admin.php
+++ admin.php
@@ -13,17 +13,10 @@ include("./Views/Admin/Layout/footer.php");
 
 // require "Views/Client/Layout/header.php";
 
-<<<<<<< HEAD
-switch ($page) {
-    case '':
-        break;
-    case "dashboard":
-        break;
-=======
+
 // switch ($page) {
 //     case "dashboard":
 //         break;
->>>>>>> 52e7aef789724f9451fb903e6f5ec8d4dd56a2d5
 
 //     case "category":
 //         break;
