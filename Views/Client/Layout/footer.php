d91a40b7a6fa95efca93e54d1f50d9151c3789ae
Quoc Bao <baolmq05@gmail.com>
2025-11-13 19:24:52 +0700

    Sửa lỗi giao diện. Xóa link flowbite

--- Views/Client/Layout/footer.php
+++ Views/Client/Layout/footer.php
@@ -107,6 +107,6 @@
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 
 </html>
