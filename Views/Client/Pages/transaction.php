6da64448cad3832ce98c3bfdd5f424ae7f46b71e
hoangbao <rowillius@gmail.com>
2025-12-15 00:53:46 +0700

    [FIX BUG] HIỂN THỊ TRANSACTION

--- Views/Client/Pages/transaction.php
+++ Views/Client/Pages/transaction.php
@@ -26,6 +26,8 @@
     }
     
     $currentFilter = isset($_GET['type']) && $_GET['type'] !== '' ? (int)$_GET['type'] : null;
+    $userRole = $_SESSION['client']['role'] ?? null;
+    $isTeacher = ($userRole === 2);
     ?>
 
     <div class="container mx-auto px-4 py-8 max-w-6xl">
@@ -52,6 +54,7 @@
                             Nạp tiền
                         </span>
                     </a>
+                    <?php if ($isTeacher): ?>
                     <a href="index.php?page=transaction&type=1" 
                        class="<?php echo $currentFilter === 1 ? 'border-yellow-500 text-yellow-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'; ?> 
                               whitespace-nowrap py-4 px-6 border-b-2 font-medium text-sm transition-colors">
@@ -62,6 +65,7 @@
                             Rút tiền
                         </span>
                     </a>
+                    <?php endif; ?>
                     <a href="index.php?page=transaction&type=2" 
                        class="<?php echo $currentFilter === 2 ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'; ?> 
                               whitespace-nowrap py-4 px-6 border-b-2 font-medium text-sm transition-colors">
